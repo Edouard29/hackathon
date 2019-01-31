1ec8d26977628eb6206f92a18188c866e2fb3123
Edouard <ferrand_edouard@orange.fr>
2019-01-31 13:37:38 +0100

    Premier tracking

---
+++ Assets/Prefabs/Joueur/Script/LeftHand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class LeftHand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = InputTracking.GetLocalPosition(XRNode.LeftHand);
+        gameObject.transform.localRotation = InputTracking.GetLocalRotation(XRNode.LeftHand);
+    }
+}
