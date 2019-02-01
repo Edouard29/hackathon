4b98383585fb8db86d1e41cb596ec9cd2dc9c385
Edouard <ferrand_edouard@orange.fr>
2019-02-01 13:37:33 +0100

    Grab the air 2

--- Assets/Prefabs/Joueur/Script/LeftHand.cs
+++ Assets/Prefabs/Joueur/Script/LeftHand.cs
@@ -6,38 +6,58 @@ using UnityEngine.XR;
 public class LeftHand : MonoBehaviour
 {
     Vector3 oldPos;
+    Transform transformPre;
     bool trigger = false;
-
+    
     public GameObject terre;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transformPre = terre.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 centre = new Vector3(0, -52, 0);
+        Vector3 centre = new Vector3(0, -21, 0);
         Vector3 pos = transform.TransformPoint(InputTracking.GetLocalPosition(XRNode.LeftHand));
         float dist = Vector3.Distance(pos, centre);
 
-        if (dist > 50)
+        Debug.Log(dist);
+
+        if (dist > 20.2)
         {
             Vector3 ventre = InputTracking.GetLocalPosition(XRNode.Head) - new Vector3(0, 0.2f, 0);
+            Vector3 angleVue = InputTracking.GetLocalRotation(XRNode.Head).eulerAngles ;
             float coefficient = 15 * (Vector3.Distance(InputTracking.GetLocalPosition(XRNode.LeftHand), ventre));
             gameObject.transform.position = InputTracking.GetLocalPosition(XRNode.LeftHand) + coefficient * (InputTracking.GetLocalPosition(XRNode.LeftHand) - ventre);
 
-            gameObject.transform.localRotation = InputTracking.GetLocalRotation(XRNode.LeftHand);
+            Vector3 rotation = InputTracking.GetLocalRotation(XRNode.LeftHand).eulerAngles;
+            rotation += new Vector3(90,180,0);
+            rotation.x *= -1;
+            rotation.y -= angleVue.y;
+            gameObject.transform.localRotation = Quaternion.Euler(rotation);
+            oldPos = gameObject.transform.position;
         } else
         {
-            Vector3 dirDep = Vector3.Normalize(gameObject.transform.position - oldPos);
-            Vector3 axeRotation = new Vector3(-dirDep.y, 0, dirDep.x);
-            terre.transform.RotateAround(centre, axeRotation, 0.1f * Mathf.Acos(Vector3.Distance(gameObject.transform.position, oldPos)/50.0f));
-        }
+            Vector3 dirDep = Vector3.Normalize(transform.TransformPoint(gameObject.transform.position) - transform.TransformPoint(oldPos));
+            Vector3 axeRotation = new Vector3(-dirDep.z, 0, dirDep.x);
+            axeRotation.Normalize();
 
-        oldPos = gameObject.transform.position;
+            if (axeRotation.magnitude > 0)
+            {
+                terre.transform.position = transformPre.position;
+                terre.transform.rotation = transformPre.rotation;
+
+                float distance = Vector3.Distance(transform.TransformPoint(gameObject.transform.position), transform.TransformPoint(oldPos));
+
+                float angle = Mathf.Asin(0.5f*distance / 20.2f);
+                terre.transform.RotateAround(centre, axeRotation, -angle);
+
+                transformPre = terre.transform;
+            }
+        }
     }
 
 }
